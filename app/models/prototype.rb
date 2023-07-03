3d10d79e3abc43eec0d4db8967175787e71bcf5b
rosicky_7th <pinestania@gmail.com>
2023-07-03 22:34:46 +0900

    一部バリデーション削除とビューファイルの修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,6 @@ class Prototype < ApplicationRecord
   validates :title, presence:true
   validates :catch_copy, presence:true
   validates :concept, presence:true
-  validates :user, presence:true
   validates :image, presence: true
 
   belongs_to :user
